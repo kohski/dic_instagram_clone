4c462ccd0231b992eeeee919f9af185a1c211bcc
木村　幸介 <kosukekimura0528@gmail.com>
2019-03-20 12:16:04 +0900

    resume contactmodel to pass heroku error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # ContactMailer.contact_mail(@user).deliver
+      ContactMailer.contact_mail(@user).deliver
       redirect_to new_session_path, notice: "sign upしました。log inしてください。"
     else
       render 'new'
