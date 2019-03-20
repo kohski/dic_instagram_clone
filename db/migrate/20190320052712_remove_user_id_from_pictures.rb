27f94a97f42405bdbc43b46483420db34b073015
木村　幸介 <kosukekimura0528@gmail.com>
2019-03-20 15:13:38 +0900

    modify migrations

--- db/migrate/20190320052712_remove_user_id_from_pictures.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserIdFromPictures < ActiveRecord::Migration[5.1]
-  def change
-    remove_foreign_key :pictures, :users
-    remove_reference :pictures, :user,index: true
-  end
-end
--- db/migrate/20190320060335_rem_user_id_from_pictures.rb
+++
@@ -1,6 +0,0 @@
-class RemUserIdFromPictures < ActiveRecord::Migration[5.1]
-  def change
-    remove_foreign_key :pictures, :users
-    remove_reference :pictures, :user,index: true
-  end
-end
